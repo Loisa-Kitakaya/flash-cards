ec3f174a557385d3b155e60c988802e5e16e5156
Loisa-Kitakaya <loisakitakaya@gmail.com>
2019-12-05 11:27:25 +0300

    |scripting|-->|adds a js script that manipulates the display of a topics description on clicking on the topic|

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+  $("#javascript-pointer").click(function() {
+    $("#javascript-description").slideToggle("#show-javascript");
+  });
+  $("#operators-pointer").click(function() {
+    $("#operators-description").slideToggle("#show-operators");
+  });
+  $("#variables-pointer").click(function() {
+    $("#variables-description").slideToggle("#show-variables");
+  });
+  $("#naming-conventions-pointer").click(function() {
+    $("#naming-conventions-description").slideToggle(
+      "#show-naming-conventions"
+    );
+  });
+  $("#functions-pointer").click(function() {
+    $("#functions-description").slideToggle("#show-functions");
+  });
+  $("#strings-pointer").click(function() {
+    $("#strings-description").slideToggle("#show-strings");
+  });
+  $("#methods-pointer").click(function() {
+    $("#methods-description").slideToggle("#show-methods");
+  });
+  $("#arguments-pointer").click(function() {
+    $("#arguments-description").slideToggle("#show-arguments");
+  });
+  $("#parameters-pointer").click(function() {
+    $("#parameters-description").slideToggle("#show-parameters");
+  });
+  $("#return-pointer").click(function() {
+    $("#return-description").slideToggle("#show-return");
+  });
+  $("#chaining-methods-pointer").click(function() {
+    $("#chaining-methods-description").slideToggle("#show-chaining-methods");
+  });
+  $("#booleans-pointer").click(function() {
+    $("#booleans-description").slideToggle("#show-booleans");
+  });
+  $("#undefined-pointer").click(function() {
+    $("#undefined-description").slideToggle("#show-undefined");
+  });
+  $("#nan-pointer").click(function() {
+    $("#nan-description").slideToggle("#show-nan");
+  });
+  $("#escape-pointer").click(function() {
+    $("#escape-description").slideToggle("#show-escape");
+  });
+  $("#alert-pointer").click(function() {
+    $("#alert-description").slideToggle("#show-alert");
+  });
+  $("#comments-pointer").click(function() {
+    $("#comments-description").slideToggle("#show-comments");
+  });
+  $("#jquery-pointer").click(function() {
+    $("#jquery-description").slideToggle("#show-jquery");
+  });
+  $("#attributes-pointer").click(function() {
+    $("#attributes-description").slideToggle("#show-attributes");
+  });
+});
